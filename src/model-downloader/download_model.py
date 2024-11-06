e39ea5843dc4666e51b6109dec31cc110c93efcc
rmr <connect2rmr@gmail.com>
2024-11-06 08:28:36 +0530

    revised vllm docker command

--- src/model-downloader/download_model.py
+++ src/model-downloader/download_model.py
@@ -5,6 +5,8 @@ from huggingface_hub import snapshot_download
 token = os.getenv("HF_API_TOKEN")
 embedding_repo_id = os.getenv("EMBEDDING_MODEL_NAME", "sentence-transformers/all-MiniLM-L6-v2")
 embedding_model_dir = os.path.join(os.getenv("DATA_DIR", "data"), "models", embedding_repo_id)
+llm_repo_id = os.getenv("LLM_MODEL_NAME")
+llm_model_dir = os.path.join(os.getenv("DATA_DIR", "data"), "models", llm_repo_id)
 DEBUG = os.getenv("DEBUG", "false").strip().lower() == "true"
 
 # downloading embeddings model
@@ -12,4 +14,12 @@ snapshot_download(
     repo_id=embedding_repo_id,
     local_dir=embedding_model_dir,  # Ensure it's downloaded to /data/models
     token=token
+)
+
+# downloading llm model
+if llm_repo_id:
+    snapshot_download(
+        repo_id=llm_repo_id,
+        local_dir=llm_model_dir,  # Ensure it's downloaded to /data/models
+        token=token
 )
